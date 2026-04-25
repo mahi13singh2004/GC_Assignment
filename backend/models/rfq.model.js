e5e60f815992ef6cc29dcfa476c2689d9de6944d
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-04-25 18:49:43 +0530

    rfq model

---
+++ backend/models/rfq.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose"
+
+const rfqSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    buyer:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    bidStartTime:{
+        type:Date,
+        required:true
+    },
+    bidCloseTime:{
+        type:String,
+        required:true
+    },
+    forcedCloseTime:{
+        type:Date,
+        required:true
+    },
+    currentCloseTime:{
+        type:Date,
+        required:true
+    },
+    triggerWindow:{
+        type:Number,
+        required:true
+    },
+    extensionDuration:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["upcoming","active","closed","force_closed"],
+        default:"upcoming"
+    }
+},
+{timestamps:true}
+)
+
+const RFQ = mongoose.model("RFQ", rfqSchema);
+export default RFQ;
