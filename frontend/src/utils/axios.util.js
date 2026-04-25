686bb80b567296054008b64263372ffc04e9f346
mahi13singh2004 <mahi13singh2004@gmail.com>
2026-04-25 23:28:26 +0530

    added backend base url

--- frontend/src/utils/axios.util.js
+++ frontend/src/utils/axios.util.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const axiosInstance=axios.create({
-    baseURL:"http://localhost:5000",
+    baseURL:"https://bid-flow-backend.onrender.com/",
     withCredentials:true
 })
 export default axiosInstance
